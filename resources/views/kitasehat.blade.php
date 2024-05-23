762ee339cead90fbec027ef189276c66f910460e
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2024-05-24 05:23:29 +0700

    perbaikan

--- resources/views/kitasehat.blade.php
+++ resources/views/kitasehat.blade.php
@@ -560,94 +560,38 @@
     <!-- ======= Doctors Section ======= -->
     <section id="doctors" class="doctors section-bg">
         <div class="container" data-aos="fade-up">
-
             <div class="section-title">
                 <h2>Doctors</h2>
-                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
-                    consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat
-                    sit in iste officiis commodi quidem hic quas.</p>
+                <p>Tim dokter kami terdiri dari ahli yang berdedikasi dan berpengalaman, siap memberikan perawatan terbaik
+                    dengan pendekatan profesional dan empati.</p>
             </div>
-
             <div class="row">
-
-                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                    <div class="member" data-aos="fade-up" data-aos-delay="100">
-                        <div class="member-img">
-                            <img src="{{ asset('medicio/assets/img/doctors/doctors-1.jpg') }}" class="img-fluid"
-                                alt="">
-                            <div class="social">
-                                <a href=""><i class="bi bi-twitter"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""><i class="bi bi-linkedin"></i></a>
+                @foreach ($dokters as $dr)
+                    <div class="col-md-3  align-items-stretch">
+                        <div class="member" data-aos="fade-up" data-aos-delay="100">
+                            <div class="member-img">
+                                @if ($dr->gender == 'P')
+                                    <img src="{{ asset('img/dr-female.jpg') }}" width="100%"
+                                        class="img-fluid max-width" alt="">
+                                @else
+                                    <img src="{{ asset('img/dr-male.jpg') }}" width="100%" class="img-fluid max-width"
+                                        alt="">
+                                @endif
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>{{ $dr->nama }}</h4>
+                                <span>{{ $dr->title }}</span>
                             </div>
                         </div>
-                        <div class="member-info">
-                            <h4>Walter White</h4>
-                            <span>Chief Medical Officer</span>
-                        </div>
                     </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                    <div class="member" data-aos="fade-up" data-aos-delay="200">
-                        <div class="member-img">
-                            <img src="{{ asset('medicio/assets/img/doctors/doctors-2.jpg') }}" class="img-fluid"
-                                alt="">
-                            <div class="social">
-                                <a href=""><i class="bi bi-twitter"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""><i class="bi bi-linkedin"></i></a>
-                            </div>
-                        </div>
-                        <div class="member-info">
-                            <h4>Sarah Jhonson</h4>
-                            <span>Anesthesiologist</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                    <div class="member" data-aos="fade-up" data-aos-delay="300">
-                        <div class="member-img">
-                            <img src="{{ asset('medicio/assets/img/doctors/doctors-3.jpg') }}" class="img-fluid"
-                                alt="">
-                            <div class="social">
-                                <a href=""><i class="bi bi-twitter"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""><i class="bi bi-linkedin"></i></a>
-                            </div>
-                        </div>
-                        <div class="member-info">
-                            <h4>William Anderson</h4>
-                            <span>Cardiology</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                    <div class="member" data-aos="fade-up" data-aos-delay="400">
-                        <div class="member-img">
-                            <img src="{{ asset('medicio/assets/img/doctors/doctors-4.jpg') }}" class="img-fluid"
-                                alt="">
-                            <div class="social">
-                                <a href=""><i class="bi bi-twitter"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""><i class="bi bi-linkedin"></i></a>
-                            </div>
-                        </div>
-                        <div class="member-info">
-                            <h4>Amanda Jepson</h4>
-                            <span>Neurosurgeon</span>
-                        </div>
-                    </div>
-                </div>
-
+                @endforeach
             </div>
-
         </div>
     </section><!-- End Doctors Section -->
 
