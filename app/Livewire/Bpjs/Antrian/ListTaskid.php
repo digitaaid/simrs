76e3812f3f15b076c3015d1b5074a4da0639e7e5
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2024-05-26 16:32:42 +0700

    task id list

--- app/Livewire/Bpjs/Antrian/ListTaskid.php
+++ app/Livewire/Bpjs/Antrian/ListTaskid.php
@@ -2,14 +2,29 @@
 
 namespace App\Livewire\Bpjs\Antrian;
 
+use App\Http\Controllers\AntrianController;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class ListTaskid extends Component
 {
-    public $kodebooking;
+    public $kodebooking, $taskid = [];
     public function cari()
     {
-        dd($this->kodebooking);
+        $this->validate([
+            'kodebooking' => 'required',
+        ]);
+        $request = new Request([
+            'kodebooking' => $this->kodebooking,
+        ]);
+        $api = new AntrianController();
+        $res  = $api->taskid_antrean($request);
+        if ($res->metadata->code == 200) {
+            $this->taskid = $res->response;
+            flash($res->metadata->message, 'success');
+        } else {
+            flash($res->metadata->message, 'danger');
+        }
     }
     public function render()
     {
