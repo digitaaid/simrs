cb79a46510bdefff71cfad21ebd7f979105fc9c7
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2024-05-31 18:28:01 +0700

    dokter import

---
+++ app/Exports/DokterExport.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Dokter;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class DokterExport implements FromCollection, WithHeadings
+{
+    public function collection()
+    {
+        return Dokter::all();
+    }
+    public function headings(): array
+    {
+        return [
+            'id',
+            'nama',
+            'kode',
+            'kodejkn',
+            'nik',
+            'user_id',
+            'idpractitioner',
+            'title',
+            'gender',
+            'sip',
+            'image',
+            'status',
+            'user',
+            'pic',
+            'created_at',
+            'updated_at',
+        ];
+    }
+}
--- resources/views/livewire/pendaftaran/navigasi-rajal.blade.php
+++ resources/views/livewire/pendaftaran/navigasi-rajal.blade.php
@@ -104,22 +104,22 @@
         </ul>
         <x-slot name="footerSlot">
             <a href="{{ route('pendaftaran.rajal') }}?tanggalperiksa={{ $antrian->tanggalperiksa }}">
-                <x-adminlte-button class="btn-xs" label="Kembali" theme="danger" icon="fas fa-arrow-left" />
+                <x-adminlte-button class="btn-xs mb-1" label="Kembali" theme="danger" icon="fas fa-arrow-left" />
             </a>
-            @if ($antrian->taskid == 1)
-                <x-adminlte-button wire:click='panggilPendaftaran' class="btn-xs" label="Panggil Pendaftaran"
+            @if ($antrian->taskid == 1 || $antrian->taskid == 2 )
+                <x-adminlte-button wire:click='panggilPendaftaran' class="btn-xs mb-1" label="Panggil Pendaftaran"
                     theme="primary" icon="fas fa-microphone" />
             @endif
             @if ($antrian->taskid == 2)
                 @if ($antrian?->kunjungan?->status)
                     <x-adminlte-button wire:click='selesaiPendaftaran'
                         wire:confirm='Apakah anda yakin antrian ini telah selesai ?' label="Selesai Pendaftaran"
-                        class="btn-xs" icon="fas fa-check" theme="success" />
+                        class="btn-xs mb-1" icon="fas fa-check" theme="success" />
                 @endif
             @endif
             <x-adminlte-button wire:click='batal'
                 wire:confirm='Apakah anda yakin ingin membatalkan antrian dan kunjungan ini ?' label="Batal"
-                class="btn-xs" icon="fas fa-times" theme="danger" />
+                class="btn-xs mb-1" icon="fas fa-times" theme="danger" />
             {{-- <a href="{{ route('batalantrian') }}?kodebooking={{ $antrian->kodebooking }}&keterangan=Dibatalkan dipendaftaran {{ Auth::user()->name }}"
                 class="btn btn-sm btn-danger withLoad">
                 <i class="fas fa-times"></i> Batal
