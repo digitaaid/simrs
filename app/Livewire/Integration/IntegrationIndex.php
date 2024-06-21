9e66c23304f7c6d627d158c7f966d513ddade355
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2024-06-21 18:00:02 +0700

    perbaikan integrasi

--- app/Livewire/Integration/IntegrationIndex.php
+++ app/Livewire/Integration/IntegrationIndex.php
@@ -2,12 +2,84 @@
 
 namespace App\Livewire\Integration;
 
+use App\Models\Integration;
 use Livewire\Component;
+use Illuminate\Support\Str;
+
 
 class IntegrationIndex extends Component
 {
+    public $integrations, $id, $name, $slug, $description, $base_url, $auth_url, $user_id, $user_key, $secret_key;
+    public $form = false;
+    public function store()
+    {
+        $this->validate([
+            'name' => 'required',
+            'slug' => 'required',
+        ]);
+        $integrasi = Integration::updateOrCreate(
+            ['id' => $this->id],
+            [
+                'name' => $this->name,
+                'slug' => Str::slug($this->slug),
+                'description' => $this->description,
+                'base_url' => $this->base_url,
+                'auth_url' => $this->auth_url,
+                'user_id' => $this->user_id,
+                'user_key' => $this->user_key,
+                'secret_key' => $this->secret_key,
+            ],
+        );
+        flash('Aplikasi Integrasi ' . $integrasi->name . ' saved successfully.', 'success');
+        $this->closeForm();
+    }
+    public function edit($id)
+    {
+        $this->form = true;
+        $permission = Integration::find($id);
+        $this->id = $permission->id;
+        $this->slug = $permission->slug;
+        $this->name = $permission->name;
+        $this->description = $permission->description;
+        $this->base_url = $permission->base_url;
+        $this->auth_url = $permission->auth_url;
+        $this->user_id = $permission->user_id;
+        $this->user_key = $permission->user_key;
+        $this->secret_key = $permission->secret_key;
+    }
+    public function openForm()
+    {
+        $this->form = true;
+        $this->name = '';
+        $this->id = '';
+        $this->slug = '';
+        $this->description = '';
+        $this->base_url = '';
+        $this->auth_url = '';
+        $this->user_id = '';
+        $this->user_key = '';
+        $this->secret_key = '';
+    }
+    public function closeForm()
+    {
+        $this->form = false;
+        $this->name = '';
+        $this->id = '';
+        $this->slug = '';
+        $this->description = '';
+        $this->base_url = '';
+        $this->auth_url = '';
+        $this->user_id = '';
+        $this->user_key = '';
+        $this->secret_key = '';
+    }
+    public function placeholder()
+    {
+        return view('components.placeholder.placeholder-text');
+    }
     public function render()
     {
+        $this->integrations = Integration::get();
         return view('livewire.integration.integration-index')
             ->title('Aplikasi Integrasi');
     }
--- resources/views/livewire/integration/integration-index.blade.php
+++ resources/views/livewire/integration/integration-index.blade.php
@@ -7,6 +7,101 @@
         </div>
     @endif
     <div class="col-md-12">
-        @livewire('integration.integration-table',['lazy' => true])
+        @if ($form)
+            <div id="editIntegration">
+                <x-adminlte-card title="Aplikasi Integrasi" theme="secondary">
+                    <form>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <input type="hidden" wire:model="id" name="id">
+                                <x-adminlte-input wire:model="name" fgroup-class="row" label-class="text-left col-3"
+                                    igroup-class="col-9" igroup-size="sm" name="name" label="Nama"
+                                    placeholder="Nama Aplikasi Integrasi" />
+                                <x-adminlte-input wire:model="slug" fgroup-class="row" label-class="text-left col-3"
+                                    igroup-class="col-9" igroup-size="sm" name="slug" label="slug"
+                                    placeholder="slug" />
+                                <x-adminlte-input wire:model="description" fgroup-class="row"
+                                    label-class="text-left col-3" igroup-class="col-9" igroup-size="sm"
+                                    name="description" label="description" placeholder="description" />
+                            </div>
+                            <div class="col-md-6">
+                                <x-adminlte-input wire:model="auth_url" fgroup-class="row" label-class="text-left col-3"
+                                    igroup-class="col-9" igroup-size="sm" name="auth_url" label="auth_url"
+                                    placeholder="auth_url" />
+                                <x-adminlte-input wire:model="base_url" fgroup-class="row" label-class="text-left col-3"
+                                    igroup-class="col-9" igroup-size="sm" name="base_url" label="base_url"
+                                    placeholder="base_url" />
+                                <x-adminlte-input wire:model="user_id" fgroup-class="row" label-class="text-left col-3"
+                                    igroup-class="col-9" igroup-size="sm" name="user_id" label="user_id"
+                                    placeholder="user_id" />
+                                <x-adminlte-input wire:model="user_key" fgroup-class="row" label-class="text-left col-3"
+                                    igroup-class="col-9" igroup-size="sm" name="user_key" label="user_key"
+                                    placeholder="user_key" />
+                                <x-adminlte-input wire:model="secret_key" fgroup-class="row"
+                                    label-class="text-left col-3" igroup-class="col-9" igroup-size="sm"
+                                    name="secret_key" label="secret_key" placeholder="secret_key" />
+                            </div>
+                        </div>
+
+                    </form>
+                    <x-slot name="footerSlot">
+                        <x-adminlte-button label="Simpan" class="btn-sm" icon="fas fa-save" wire:click="store"
+                            wire:confirm="Apakah anda yakin ingin menyimpan Aplikasi Integrasi ?" theme="success" />
+                        <x-adminlte-button wire:click='closeForm' class="btn-sm" label="Tutup" theme="danger"
+                            icon="fas fa-times" />
+                    </x-slot>
+                </x-adminlte-card>
+            </div>
+        @endif
+        <x-adminlte-card title="Table Aplikasi Integrasi" theme="secondary">
+            @if (flash()->message)
+                <x-adminlte-alert theme="{{ flash()->class }}" title="{{ flash()->class }} !" dismissable>
+                    {{ flash()->message }}
+                </x-adminlte-alert>
+            @endif
+            <div class="row">
+                <div class="col-md-8">
+                    <x-adminlte-button wire:click='openForm' class="btn-sm mb-3" label="Add Permission" theme="success"
+                        icon="fas fa-user-plus" />
+                </div>
+                <div class="col-md-6">
+                </div>
+            </div>
+            <table class="table text-nowrap table-sm table-hover table-bordered table-responsive-xl mb-3">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nama</th>
+                        <th>Description</th>
+                        <th>Action</th>
+                        <th>User</th>
+                        <th>BaseUrl</th>
+                        <th>AuthUrl</th>
+                        <th>UserKey</th>
+                        <th>SecretKey</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($integrations as $item)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->description }}</td>
+                            <td>
+                                <a href="#editIntegration">
+                                    <x-adminlte-button label="Edit" class="btn-xs" icon="fas fa-edit"
+                                        wire:click="edit({{ $item->id }})" theme="warning" />
+                                </a>
+                            </td>
+                            <td>{{ $item->user_id }}</td>
+                            <td>{{ $item->base_url }}</td>
+                            <td>{{ $item->auth_url }}</td>
+                            <td>{{ $item->user_key }}</td>
+                            <td>{{ $item->secret_key }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </x-adminlte-card>
     </div>
 </div>
