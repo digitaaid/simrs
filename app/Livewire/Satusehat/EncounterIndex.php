e278b910d778db29530dbbdbad3ece93c1f30158
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2024-07-05 14:27:23 +0700

    perbaikan satu sehat

---
+++ app/Livewire/Satusehat/EncounterIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Satusehat;
+
+use Livewire\Component;
+
+class EncounterIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.satusehat.encounter-index');
+    }
+}
---
+++ app/Livewire/Satusehat/TokenIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Satusehat;
+
+use Livewire\Component;
+
+class TokenIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.satusehat.token-index')->title('Token Satu Sehat');
+    }
+}
