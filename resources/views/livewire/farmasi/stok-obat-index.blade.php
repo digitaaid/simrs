4c3c07761f60540ff2cdb02da281a12ba83d65cb
marwandhiaurrahman@gmail.com <marwandhiaurrahman@gmail.com>
2024-09-16 07:22:32 +0700

    total harga keluar

--- resources/views/livewire/farmasi/stok-obat-index.blade.php
+++ resources/views/livewire/farmasi/stok-obat-index.blade.php
@@ -1,5 +1,33 @@
     <div class="row">
         <div class="col-md-12">
+            <x-adminlte-card title="Informasi Obat Masuk" theme="primary">
+                <div class="row ">
+                    <div class="col-md-8">
+                        <x-adminlte-button wire:click='openForm' class="btn-sm mb-2" label="Tambah Obat Masuk"
+                            theme="success" icon="fas fa-plus" />
+                    </div>
+                    <div class="col-md-4">
+                    </div>
+                </div>
+                <table class="table text-nowrap table-sm table-hover table-bordered mb-3">
+                    <thead>
+                        <tr>
+                            <th>Tgl Waktu</th>
+                            <th>Kode</th>
+                            <th>Tgl Expire</th>
+                            <th>Nama</th>
+                            <th>Harga/Kemasan</th>
+                            <th>Diskon</th>
+                            <th>Jumlah</th>
+                            <th>Harga Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </x-adminlte-card>
+        </div>
+        <div class="col-md-12">
             <x-adminlte-card title="Informasi Pemakaian Obat" theme="primary">
                 <table class="table text-nowrap table-sm table-hover table-bordered mb-3">
                     <thead>
@@ -34,8 +62,15 @@
                             </tr>
                         @endforeach
                     </tbody>
+                    <tfoot>
+                        <tr>
+                            <th colspan="5">Total</th>
+                            <th>{{ $resepfarmasidetails->sum('jumlah') }}</th>
+                            <th>{{ money($resepfarmasidetails->sum('subtotal'), 'IDR') }}</th>
+                            <th colspan="4"></th>
+                        </tr>
+                    </tfoot>
                 </table>
-
             </x-adminlte-card>
         </div>
     </div>
