3c51cb74d712d9956b63863c9ee2a100fd3cf89c
marwandhiaurrahman@gmail.com <marwandhiaurrahman@gmail.com>
2024-09-29 12:07:27 +0700

    perbaikan

--- app/Console/Commands/PengigatAbsensi.php
+++ app/Console/Commands/PengigatAbsensi.php
@@ -33,16 +33,18 @@ class PengigatAbsensi extends Command
             $user = User::get();
             $api = new WhatsappController();
             $absensis = ShiftPegawai::where('tanggal', now()->format('Y-m-d'))->get();
-            dd($absensis);
             foreach ($absensis as $absensi) {
-                if ($absensi->user) {
-                    $request['number'] = $absensi->user?->phone;
-                    $request['message'] = "Test Percobaan. Selamat pagi 😊🙏\nSebagai pengingat anda hari ini memiliki jadwal absensi " . $absensi->nama_shift . " pukul " . $absensi->jam_masuk . "-" . $absensi->jam_pulang . " . Jangan lupa absensi tetap waktu ya. 😉\nSemoga semoga hari ini segala urusan kita diperlancar 🤲😊\n\nklinikkitasehat.com";
-                    $res =  $api->send_message($request);
-                    $this->info($absensi->user?->name . ' terkirim');
-                }
+                $this->info($absensi->user?->name . ' terkirim');
             }
-            $this->info('Pesan Whatsapp Pengingat Absensi telah dikirim!');
+            // foreach ($absensis as $absensi) {
+            //     if ($absensi->user) {
+            //         $request['number'] = $absensi->user?->phone;
+            //         $request['message'] = "Test Percobaan. Selamat pagi 😊🙏\nSebagai pengingat anda hari ini memiliki jadwal absensi " . $absensi->nama_shift . " pukul " . $absensi->jam_masuk . "-" . $absensi->jam_pulang . " . Jangan lupa absensi tetap waktu ya. 😉\nSemoga semoga hari ini segala urusan kita diperlancar 🤲😊\n\nklinikkitasehat.com";
+            //         $res =  $api->send_message($request);
+            //         $this->info($absensi->user?->name . ' terkirim');
+            //     }
+            // }
+            // $this->info('Pesan Whatsapp Pengingat Absensi telah dikirim!');
         } catch (\Throwable $th) {
             $this->error($th->getMessage());
         }
