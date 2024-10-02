d8044ebf6ac84fb3e59bbf39b669d7dc6d057d44
marwandhiaurrahman@gmail.com <marwandhiaurrahman@gmail.com>
2024-10-02 23:54:17 +0700

    test wa

--- app/Http/Controllers/AntrianController.php
+++ app/Http/Controllers/AntrianController.php
@@ -642,7 +642,7 @@ class AntrianController extends ApiController
         $res = $this->tambah_antrean($request);
         if ($res->metadata->code == 200) {
             $request['status'] = 1;
-            Antrian::create($request->all());
+            $antrian = Antrian::create($request->all());
             $data = [
                 'nomorantrean' => $request->nomorantrean,
                 'angkaantrean' => $request->angkaantrean,
@@ -657,13 +657,21 @@ class AntrianController extends ApiController
                 'kuotanonjkn' => $request->kuotanonjkn,
                 'keterangan' => $request->keterangan,
             ];
-            $response = [
-                'metadata' => [
-                    'message' => 'Ok',
-                    'code' =>  200,
-                ],
-            ];
-            return $response;
+            try {
+                $wa = new WhatsappController();
+                $request['number'] = 120363344588087064;
+                $request['message'] = "Antrian berhasil melalui " . $antrian->method . "\nNama : *" . $antrian->nama . "* \nKodebooking : " . $antrian->kodebooking . "\nNomor : " . $antrian->nomorantrian . "\nTanggal : " . $antrian->tanggalperiksa . "\nDokter : " . $antrian->namadokter . "\nJam Praktek : " . $antrian->jampraktek;
+                $wa->send_message_group($request);
+            } catch (\Throwable $th) {
+                //throw $th;
+            }
+            // $response = [
+            //     'metadata' => [
+            //         'message' => 'Ok',
+            //         'code' =>  200,
+            //     ],
+            // ];
+            // return $response;
             return $this->sendResponse($data, 200);
         } else {
             return $this->sendError($res->metadata->message, 400);
