d45c3cdd21448effd082d48d78a96efaac7c20e4
marwandhiaurrahman@gmail.com <marwandhiaurrahman@gmail.com>
2024-10-17 09:40:38 +0700

    perbaikan jadwal iperas

--- app/Http/Controllers/JadwalOperasiController.php
+++ app/Http/Controllers/JadwalOperasiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -16,7 +17,23 @@ class JadwalOperasiController extends ApiController
         if ($validator->fails()) {
             return $this->sendError($validator->errors()->first(),  201);
         }
-        return $this->sendError("Klinik belum memiliki jadwal operasi.", 400);
+        $jadwals = [];
+        for ($i = 1; $i < 3; $i++) {
+            $jadwals[] = [
+                "kodebooking" => 'OK' . Carbon::now()->format('Ymd') . $i,
+                "tanggaloperasi" => Carbon::now()->format('Y-m-d'),
+                "jenistindakan" => 'test briding jadwal operasi',
+                "kodepoli" => 'BED',
+                "namapoli" =>  "BEDAH",
+                "terlaksana" =>  0,
+                "nopeserta" =>  '0000067026778',
+                "lastupdate" => Carbon::now()->timestamp * 1000,
+            ];
+        }
+        $response = [
+            "list" => $jadwals
+        ];
+        return $this->sendResponse($response,  200);
     }
     public function jadwal_operasi_pasien(Request $request)
     {
@@ -26,6 +43,20 @@ class JadwalOperasiController extends ApiController
         if ($validator->fails()) {
             return $this->sendError($validator->errors()->first(),  201);
         }
-        return $this->sendError("Klinik belum memiliki jadwal operasi.", 400);
+        $jadwals = [];
+        for ($i = 1; $i < 3; $i++) {
+            $jadwals[] = [
+                "kodebooking" => 'OK' . Carbon::now()->format('Ymd') . $i,
+                "tanggaloperasi" => Carbon::now()->format('Y-m-d'),
+                "jenistindakan" => 'test briding jadwal operasi',
+                "kodepoli" => 'BED',
+                "namapoli" =>  "BEDAH",
+                "terlaksana" => 0,
+            ];
+        }
+        $response = [
+            "list" => $jadwals
+        ];
+        return $this->sendResponse($response,  200);
     }
 }
