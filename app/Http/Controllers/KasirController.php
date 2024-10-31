ead0712f7fc419d5159deed3562fe334d23980a7
marwandhiaurrahman@gmail.com <marwandhiaurrahman@gmail.com>
2024-10-31 09:18:56 +0700

    nota kasir rajal

--- app/Http/Controllers/KasirController.php
+++ app/Http/Controllers/KasirController.php
@@ -31,6 +31,27 @@ class KasirController extends Controller
     public function print_notarajalf($kodebooking)
     {
         $kunjungan = Kunjungan::where('kode', $kodebooking)->first();
+        $sep = null;
+        $peserta = null;
+        if ($kunjungan->sep) {
+            $vclaim = new VclaimController();
+            $request = new Request([
+                'noSep' => $kunjungan->sep
+            ]);
+            $res = $vclaim->sep_nomor($request);
+            if ($res->metadata->code == 200) {
+                $sep = $res->response;
+                $peserta = $sep->peserta;
+                $request['nomorkartu'] = $peserta->noKartu;
+                $request['tanggal'] = $sep->tglSep;
+                $res = $vclaim->peserta_nomorkartu($request);
+                if ($res->metadata->code == 200) {
+                    $peserta = $res->response->peserta;
+                }
+            } else {
+                return $res->metadata->message;
+            }
+        }
         $resepobat = $kunjungan->resepobat;
         $resepobatdetails = $kunjungan->resepfarmasidetails;
         $qrurl = QrCode::format('png')->size(100)->generate(route('print.notarajal', $kunjungan->kode));
@@ -42,8 +63,8 @@ class KasirController extends Controller
         $qrttdpasien = QrCode::format('png')->size(150)->generate($kunjungan->nama);
         $ttdpasien = "data:image/png;base64," . base64_encode($qrttdpasien);
         // return view('print.pdf_notarajal', compact('resepobatdetails', 'resepobat', 'antrian','url));
-        $pdf = Pdf::loadView('print.pdf_notarajal', compact('resepobatdetails', 'resepobat', 'kunjungan', 'url', 'ttdpetugas', 'ttdpasien'));
-        return $pdf->stream($kunjungan->nama . '.pdf');
+        $pdf = Pdf::loadView('print.pdf_notarajal', compact('resepobatdetails', 'sep', 'peserta', 'resepobat', 'kunjungan', 'url', 'ttdpetugas', 'ttdpasien'));
+        return $pdf->stream($kunjungan->sep ?? $kunjungan->nama . '.pdf');
     }
     public function print_nota_penjualanobat($kode)
     {
