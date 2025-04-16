154a8952a8765f735b84863e4f7ffe2371d8fda7
marwandhiaurrahman@gmail.com <marwandhiaurrahman@gmail.com>
2025-04-16 09:33:24 +0700

    tampilan display ranap

--- resources/views/livewire/ranap/display-ranap.blade.php
+++ resources/views/livewire/ranap/display-ranap.blade.php
@@ -55,8 +55,42 @@
                 *Data terupdate {{ now() }}
             </x-adminlte-card>
         </div>
-        <div class="col-md-6">
-
+        <div class="col-md-4">
+            <x-adminlte-card body-class="p-1 m-0">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" height="100%" width="100%"
+                                src="{{ asset('bpjs/wajibmjkn.jpg') }}" alt="First slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>...</h5>
+                                <p>...</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" height="100%" width="100%"
+                                src="{{ asset('bpjs/caramjkn.jpg') }}" alt="First slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>...</h5>
+                                <p>...</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </x-adminlte-card>
         </div>
     </div>
 </div>
