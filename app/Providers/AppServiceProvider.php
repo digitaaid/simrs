cef3c92ab61e89ce7287c8a5ce3e428bd32a7423
marwandhiaurrahman@gmail.com <marwandhiaurrahman@gmail.com>
2025-04-22 21:54:38 +0700

    perbaikan logo img

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
         if ($pengaturan = Pengaturan::first()) {
             Config::set('adminlte.title',  $pengaturan->nama);
             Config::set('adminlte.logo', '<b>' . $pengaturan->nama . '</b>');
-            Config::set('adminlte.logo_img', $pengaturan->logo_icon ? asset('storage/pengaturan/' . $pengaturan->logo_icon) : asset('vendor/adminlte/dist/img/AdminLTELogo.png'));
+            Config::set('adminlte.logo_img', $pengaturan->logo_icon ? 'storage/pengaturan/' . $pengaturan->logo_icon : 'vendor/adminlte/dist/img/AdminLTELogo.png');
             Config::set('adminlte.auth_img', $pengaturan->auth_img ? asset('storage/pengaturan/' . $pengaturan->auth_img) : asset('vendor/adminlte/dist/img/AdminLTELogo.png'));
         }
     }
