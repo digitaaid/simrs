f9d995c67dc834fcf86b28f173a2165db9820320
marwandhiaurrahman@gmail.com <marwandhiaurrahman@gmail.com>
2025-05-05 06:53:31 +0700

    perbaikan asset

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,10 +26,10 @@ class AppServiceProvider extends ServiceProvider
             Config::set('adminlte.logo', '<b>' . $pengaturan->nama . '</b>');
             Config::set('adminlte.logo_img', $pengaturan->logo_icon ? 'storage/pengaturan/' . $pengaturan->logo_icon : 'vendor/adminlte/dist/img/AdminLTELogo.png');
             Config::set('adminlte.auth_img', $pengaturan->auth_img ? asset('storage/pengaturan/' . $pengaturan->auth_img) : asset('vendor/adminlte/dist/img/AdminLTELogo.png'));
-            Config::set('adminlte.logo_karcis', $pengaturan->logo_karcis ? asset('storage/pengaturan/' . $pengaturan->logo_karcis) : asset('vendor/adminlte/dist/img/AdminLTELogo.png'));
+            Config::set('adminlte.logo_karcis', $pengaturan->logo_karcis ? 'storage/pengaturan/' . $pengaturan->logo_karcis : 'vendor/adminlte/dist/img/AdminLTELogo.png');
             Config::set('adminlte.anjungan_color', $pengaturan->anjungan_color ?? 'green');
-            Config::set('adminlte.anjungan_qr', $pengaturan->anjungan_qr ? asset('storage/pengaturan/' . $pengaturan->anjungan_qr) : asset('img/image-placeholder.jpg'));
-            Config::set('adminlte.anjungan_img_info', $pengaturan->anjungan_img_info ? asset('storage/pengaturan/' . $pengaturan->anjungan_img_info) : asset('img/image-placeholder.jpg'));
+            Config::set('adminlte.anjungan_qr', $pengaturan->anjungan_qr ? 'storage/pengaturan/' . $pengaturan->anjungan_qr : 'img/image-placeholder.jpg');
+            Config::set('adminlte.anjungan_img_info', $pengaturan->anjungan_img_info ? 'storage/pengaturan/' . $pengaturan->anjungan_img_info : 'img/image-placeholder.jpg');
         }
     }
 }
